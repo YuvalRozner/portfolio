8da899029ddc8dfb58fe99312300715e577113cb
Yuval Rozner <yuvalrozner98@gmail.com>
2024-12-31 10:01:35 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import TopBar from './components/headerAndFooter/topBar';
 import Footer from './components/headerAndFooter/footer';
 import Header from './components/headerAndFooter/header';
@@ -8,22 +8,6 @@ import ProjectsView from './components/projects/projectsView';
 
 function App() {
   const [viewMode, setViewMode] = useState('cards');
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    // const functionUrl = 'https://us-central1-yuval-portfolio.cloudfunctions.net/app';
-    const functionUrl = 'https://flask-app-244769483465.us-central1.run.app/';
-
-    fetch(functionUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.text();
-      })
-      .then(data => setMessage(data))
-      .catch(error => console.error('Error fetching data:', error.message));
-  }, []);
 
   const toggleViewMode = () => {
     setViewMode((prevMode) => (prevMode === 'cards' ? 'list' : 'cards'));
@@ -34,10 +18,6 @@ function App() {
     <div className="portfolio" style={{ fontFamily: 'Arial, sans-serif', color: '#333', lineHeight: '1.6' }}>
       <TopBar />
       <Header />
-      <div className="App">
-        <h1>Message from Server:</h1>
-        <p>{message}</p>
-      </div>
       <About />
       <ProjectsView viewMode={viewMode} toggleViewMode={toggleViewMode} />
       <Contact />
